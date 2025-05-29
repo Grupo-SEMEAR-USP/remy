61b207cce97e6375c17f3b51bf9e57eca8be40f0
Henrique Figueiredo <henriquefigueiredo@usp.br>
2025-05-29 12:50:57 -0300

    calcula rpm
    Coloquei a saida pwm e um codigo que calcula o rpm

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -1,19 +1,20 @@
 #define ha_pin 18
 #define hb_pin 19
 #define hc_pin 21
+#define pwm 4
 
-volatile bool mudou_ha = false;
-volatile bool mudou_hb = false;
-volatile bool mudou_hc = false;
+volatile long etapas = 0;
+
+volatile bool mudou_hall = false;
 
 void IRAM_ATTR mudar_ha() {
-  mudou_ha = true; }
+  mudou_hall = true; }
 
 void IRAM_ATTR mudar_hb() {
-  mudou_hb = true; }
+  mudou_hall = true; }
 
 void IRAM_ATTR mudar_hc() {
-  mudou_hc = true; }
+  mudou_hall = true; }
 
 void setup() {
   
@@ -22,6 +23,7 @@ void setup() {
   pinMode(ha_pin, INPUT);
   pinMode(hb_pin, INPUT);
   pinMode(hc_pin, INPUT);
+  pinMode(pwm, OUTPUT);
 
   attachInterrupt(digitalPinToInterrupt(ha_pin), mudar_ha, CHANGE);
   attachInterrupt(digitalPinToInterrupt(hb_pin), mudar_hb, CHANGE);
@@ -31,42 +33,45 @@ void setup() {
 
 void loop() {
 
+  analogWrite(pwm, 10);
+
+  static unsigned long antes = millis();
+  unsigned long agora = millis();
+  static long graus = 0;
+
   int ha, hb, hc;
-  int contador = 0;
 
-  if (mudou_ha) {
-    mudou_ha = false;
-    contador++;
+  //sempre que um dos halls muda de valor
+  if (mudou_hall) {
+    mudou_hall = false;
+    etapas++;
+    graus += 4;
+
     ha = digitalRead(ha_pin);
-    Serial.print(ha );
-    Serial.print(hb );
-    Serial.println(hc);
-    Serial.print("Numero de mudancas: ");
-    Serial.println(contador);
-  }
-
-  if (mudou_hb) {
-    mudou_hb = false;
-    contador++;
     hb = digitalRead(hb_pin);
-    Serial.print(ha );
-    Serial.print(hb );
-    Serial.println(hc);
-    Serial.print("Numero de mudancas: ");
-    Serial.println(contador);
-  }
-
-  if (mudou_hc) {
-    mudou_hc = false;
-    contador++;
     hc = digitalRead(hc_pin);
+
     Serial.print(ha );
     Serial.print(hb );
     Serial.println(hc);
-    Serial.print("Numero de mudancas: ");
-    Serial.println(contador);
-
+    Serial.print("Graus girados: ");
+    Serial.println(graus);
   }
+
+  // Cálculo de velocidade (a cada 1 segundo)
+  if (agora - antes >= 1000) {
+    antes = agora;
+    float rpm_eletrico = (etapas / 6.0) * 60.0; // 6 etapas por rotação elétrica
+    float rpm_mecanico = rpm_eletrico / 15.0;    // 15 pares de polos
+
+    Serial.print("Velocidade: ");
+    Serial.print(rpm_mecanico);
+    Serial.println(" RPM");
+
+    etapas = 0;
+  }
+
+  
 }
 
 
